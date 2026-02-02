c0bcc343e6a6db6cb2cd35330250b767e54248bf
jeremyschied <jeremyschied@gmail.com>
2026-02-01 19:28:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ app.use(express.json());
 
 const TWIN_WEBHOOK = 'https://build.twin.so/triggers/a7558d78-64df-41df-8538-86012d24ac56/webhook';
 
-app.post('/slack/events', async (req, res) => { const body = req.body;
+app.post('/slack/events', async (req, res) => {
+  const body = req.body;
+  console.log('Payload:', JSON.stringify(body));
+
+
+                                              
 
 if (body.type === 'url_verification') { console.log('URL verification challenge received'); return res.json({ challenge: body.challenge }); }
 
