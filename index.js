f8996c737308b984bd0bf23ee6485d4b48bd9d24
jeremyschied <jeremyschied@gmail.com>
2026-02-03 21:39:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const BIG_BRAIN_WEBHOOK = 'https://build.twin.so/triggers/ee1d2a2d-6652-4a84-a490-b2d9773e33fb/webhook';
+const BIG_BRAIN_WEBHOOK = 'https://build.twin.so/triggers/ee1d2a2d-6652-4a84-a490-b2d9773e33fb/webhook'; const BIG_BRAIN_IN_CHANNEL = 'C0ACXEDBURJ';
 
 app.post('/slack/events', async function(req, res) { const body = req.body; console.log('Payload:', JSON.stringify(body));
 
